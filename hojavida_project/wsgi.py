43c52eeb7fdfdd1413521feb340ccf5d7a1b63cf
MirosXD <miroslavxde@gmail.com>
2026-01-27 22:07:14 -0500

    Forzando creacion de admin desde wsgi

--- hojavida_project/wsgi.py
+++ hojavida_project/wsgi.py
@@ -3,3 +3,18 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hojavida_project.settings")
 application = get_wsgi_application()
+
+# Al final de wsgi.py
+from django.contrib.auth.models import User
+
+try:
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@ejemplo.com', 'Miroslav03')
+        print("✅ ADMIN CREADO DESDE WSGI")
+    else:
+        user = User.objects.get(username='admin')
+        user.set_password('Miroslav03')
+        user.save()
+        print("🔄 PASSWORD DE ADMIN ACTUALIZADA")
+except Exception as e:
+    print(f"Error creando admin: {e}")
